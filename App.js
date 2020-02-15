299fb9b70051e9ee4325a56a5f6cef19252a6790
Chohra <fm_chohra@esi.dz>
2020-02-15 15:07:28 +0100

    adding redux

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import Router from './src/navigation/router';
 
-const App = () => <Router />;
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import configureStore from './src/redux/configureStore';
+
+const {persistor, store} = configureStore();
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <Router />
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
